f655177a075f0996727c10220f1103db76040d74
lionelholzapfelbp <123163050+lionelholzapfelbp@users.noreply.github.com>
2023-01-30 23:50:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+
 streamlit.title('My parents new healthy Diner')
 
 streamlit.header(' 🥣 Breakfast Menu')
@@ -9,7 +15,7 @@ streamlit.text('🥑🍞 Avocadeo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
-import pandas 
+#import pandas 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a picklist so they can pick the fruit they want to include
@@ -19,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-import requests
+#import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user entered ', fruit_choice)
